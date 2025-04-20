1863b01f321f85af7e1764608bbf72907188c538
Seul Lee <ftk3187@athena.athena.research.northwestern.edu>
2025-04-20 12:44:46 -0500

    250420

--- fcnn_constr/Policy.py
+++ fcnn_constr/Policy.py
@@ -18,17 +18,13 @@ def DPC_loss(model_output: torch.Tensor, target: torch.Tensor, u_output: torch.T
     squared_diff = u_diff ** 2
 
     model_output_x2 = model_output[:, :, 1]
-    low_violation = F.relu(c_fut[:, :, 0] - model_output_x2)
-    up_violation = F.relu(model_output_x2 - c_fut[:, :, 1])
-
-    low_violation = low_violation + 0.5 * (low_violation > 0).float()
-    up_violation = up_violation + 0.5 * (up_violation > 0).float()
-
-    constraint_loss = (low_violation ** 2) + (up_violation ** 2)
+    low_violation = F.relu(c_fut[:, :, 0] - model_output_x2) ** 2 
+    up_violation = F.relu(model_output_x2 - c_fut[:, :, 1]) ** 2 
+    constraint_loss = low_violation + up_violation  
 
     tracking_loss = torch.sqrt(errors.mean())
-    smoothness_loss = 10 * torch.sqrt(squared_diff.mean())
-    constraint_loss = torch.sqrt(constraint_loss.mean())
+    smoothness_loss = 0.1*torch.sqrt(squared_diff.mean())
+    constraint_loss = 3*torch.sqrt(constraint_loss.mean())
 
     total_loss = tracking_loss + smoothness_loss + constraint_loss
 
@@ -36,35 +32,6 @@ def DPC_loss(model_output: torch.Tensor, target: torch.Tensor, u_output: torch.T
         return total_loss, tracking_loss.item(), smoothness_loss.item(), constraint_loss.item()
     else:
         return total_loss
-    
-def DPC_loss_notused(model_output: torch.Tensor, target: torch.Tensor, u_output: torch.Tensor, c_fut: torch.Tensor, return_all=False):
-    model_output = model_output.median(dim=-1).values
-    model_output_x1 = model_output[:, :, 0]
-    target_x1 = target[:, :, 0]
-    errors = (target_x1 - model_output_x1) ** 2
-
-    u_diff = u_output[:, 1:, :] - u_output[:, :-1, :]
-    squared_diff = u_diff ** 2
-
-    model_output_x2 = model_output[:, :, 1]
-    low_violation_mask = model_output_x2 < c_fut[:, :, 0]
-    up_violation_mask = model_output_x2 > c_fut[:, :, 1]
-    penalty = 5.0
-    low_violation = penalty * low_violation_mask.float()
-    up_violation = penalty * up_violation_mask.float()
-    constraint_violation = low_violation + up_violation
-
-    constraint_loss = constraint_violation.mean() 
-    tracking_loss = torch.sqrt(errors.mean())
-    smoothness_loss = 10 * torch.sqrt(squared_diff.mean())
-
-    total_loss = tracking_loss + smoothness_loss + constraint_loss
-
-    if return_all:
-        return total_loss, tracking_loss.item(), smoothness_loss.item(), constraint_loss.item()
-    else:
-        return total_loss
-
 
 
 class DPC_PolicyNN(nn.Module):
